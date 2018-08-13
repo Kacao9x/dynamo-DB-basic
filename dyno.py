06e446d0ea779e41fdad9c98f9df9578a8871a0d
Kacao Cao <kacao@iastate.edu>
2018-08-13 10:40:13 -0400

    made modules for basic database function: get, put, add, delete

--- dyno.py
+++ dyno.py
@@ -5,8 +5,13 @@ from boto3.dynamodb.conditions import Key
 dynamodb = resource('dynamodb')
 table = dynamodb.Table('nis3')
 
-
+# read the setting of table
 def get_table_metadata( tablename ):
+    """
+    Get some setting about the chosen table
+    :param tablename:
+    :return:
+    """
     table = dynamodb.Table( tablename )
 
     return {
@@ -16,11 +21,42 @@ def get_table_metadata( tablename ):
     }
 
 def read_table_item( table_name, pk_name, pk_value ):
+    """
+    :param table_name:
+    :param pk_name: column/header name to scan
+    :param pk_value:
+    :return: item read by primary key
+    """
     table = dynamodb.Table( table_name )
     response = table.get_item( Key = {pk_name: pk_value})
     return response
 
-read_table_item( 'nis3', 'name', 'leaf')
+def add_item( table_name, col_dict ):
+    """
+    :param table_name:
+    :param pk_name:
+    :param pk_value:
+    :return: add one item (row) to table. col_dict is a dictionary
+    {col_name:value}
+    """
+    table = dynamodb.Table( table_name, col_dict )
+    response = table.put_item( Item = col_dict )
+    return response
+
+def delete_item( table_name, pk_name, pk_value ):
+    """
+    Delete an item (row) from its primary key
+    :param table_name:
+    :param pk_name:
+    :param pk_value:
+    :return:
+    """
+    return response
+
+
+response = read_table_item( 'nis3', 'name', 'leaf')
+print ( response )
+print ( response['Item'] )
 # numitem, key, status = get_table_metadata( 'mytesttable')
 # print ( 'item values: {}, {}, {}'.format(numitem, key, status))
 
